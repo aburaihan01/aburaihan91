d39324a0742d1ffc1f6a439fa57b8ab0ec23571a
aburaihan01 <raihan9127@gmail.com>
2024-05-13 22:21:51 +0600

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,46 @@
+/*program to calculate electricity bill under following conditions
+first 20 units - Rs 80
+next 80 units - Rs 5 per unit
+next 100 units - Rs 7 per unit
+beyond 200 units - Rs 10 per unit
+addition Rs 50 charge for all users
+*/
+
+    #include <stdio.h>
+    #include <stdbool.h>
+    #include <conio.h>
+
+    int main()
+{
+    int unit, mc=80, sc=50,tc;
+    printf("Enter the units consumed: \t");
+    scanf("%d",&unit);
+{
+    tc-mc+sc;
+    if(unit>20)
+    {
+
+    if(unit<=100)
+
+
+{
+    if(unit>100&&unit<=200);
+
+
+{
+    tc+=((80*5)+((unit -100)*7));
+
+
+
+    if(unit >200)
+    {
+    tc+=((80*5)+(100*7)+((unit-200)*10));
+    }
+
+}
+printf("\n\nThe total charge = %d*", tc);
+return 0;
+}
+    }
+}
+}
